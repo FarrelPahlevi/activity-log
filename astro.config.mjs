7ccc13d1b1e16af672858e31d06a58ef93f2c907
Farrel Pahlevi <farrel.p2235@gmail.com>
2026-07-16 13:55:35 +0700

    update site url ke netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://farrel-activity-log.netlify.app',
+  site: 'https://farrelactivitylog.netlify.app',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
